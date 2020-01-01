f5da426c4f411c17853b03181582f7dcccc17d10
Bruce Ediger <bediger8@gmail.com>
2020-01-01 06:25:58 -0700

    Deduped dictionary

--- runserver.go
+++ runserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"jumble/dictionary"
 	"jumble/srvr"
@@ -16,11 +17,18 @@ func main() {
 		log.Fatalf("Problem reading %q: %vn", os.Args[1], err)
 	}
 
+	before := time.Now()
+	dict := dictionary.Build(buffer)
+	dict.Dedupe()
+	fmt.Printf("Dictionary construction %v\n", time.Since(before))
+
 	srv := &srvr.Srvr{
 		Router:    http.NewServeMux(),
-		FindWords: dictionary.Build(buffer),
+		FindWords: dict,
 	}
+
 	srv.Routes()
+
 	s := &http.Server{
 		Addr:           ":8012",
 		Handler:        srv.Router,
