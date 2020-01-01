6639723aa1240ec015236c0b6824d36fe03bbfc2
Bruce Ediger <bediger8@gmail.com>
2019-12-31 19:23:11 -0700

    Break apart func Build
    Call Build with a buffer of lines.
    Break out de-duplication into its own method.

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -2,7 +2,6 @@ package dictionary
 
 import (
 	"bytes"
-	"io/ioutil"
 	"sort"
 	"strings"
 )
@@ -10,15 +9,14 @@ import (
 // keys are alphabetized words, values are the words
 type Dictionary map[string][]string
 
-func Build(filename string) (Dictionary, error) {
-	buffer, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
+// Build fills in a Dictionary from a buffer, which
+// should constitute all the lines from a file of words,
+// one word per line.
+func Build(buffer []byte) Dictionary {
 	dict := Dictionary(make(map[string][]string))
 	lines := bytes.Fields(buffer)
 	for _, word := range lines {
-		if w, a, saveit := alphabetizer(word); saveit {
+		if w, a, saveit := Alphabetizer(word); saveit {
 			if _, ok := dict[a]; ok {
 				dict[a] = append(dict[a], string(w))
 				continue
@@ -28,9 +26,18 @@ func Build(filename string) (Dictionary, error) {
 			dict[a] = words
 		}
 	}
-	// eliminate duplicate words from list of words that
-	// alphabetize to a given string.
+	return dict
+}
+
+// Dedupe eliminates duplicate words from list of words that
+// alphabetize to a given string.
+func (dict Dictionary) Dedupe() (int, int) {
+	maxbefore := 0
+	max := 0
 	for alphabetized, words := range dict {
+		if n := len(words); n > maxbefore {
+			maxbefore = n
+		}
 		d := make(map[string]bool)
 		for _, word := range words {
 			d[word] = true
@@ -39,9 +46,12 @@ func Build(filename string) (Dictionary, error) {
 		for word, _ := range d {
 			newwords = append(newwords, word)
 		}
+		if n := len(newwords); n > max {
+			max = n
+		}
 		dict[alphabetized] = newwords
 	}
-	return dict, nil
+	return maxbefore, max
 }
 
 type RuneArray []rune
@@ -50,11 +60,11 @@ func (r RuneArray) Len() int           { return len(r) }
 func (r RuneArray) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
 func (r RuneArray) Less(i, j int) bool { return r[i] < r[j] }
 
-// alphabetizer receives a word as an array of bytes, returns
+// Alphabetizer receives a word as an array of bytes, returns
 // a lowercase word, the word with all runes alphabetized,
 // a bool indicating whether to save this word or not.
 // Don't save words with apostrophes, commas, etc in them.
-func alphabetizer(rawWord []byte) (string, string, bool) {
+func Alphabetizer(rawWord []byte) (string, string, bool) {
 	word := strings.ToLower(string(rawWord))
 	runes := []rune(word)
 	if strings.ContainsAny(word, `'"!,:;.?/`) {
