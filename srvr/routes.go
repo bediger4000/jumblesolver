7721d64919fefb649fd2e0bd891db943493fd87f
Bruce Ediger <bediger8@gmail.com>
2020-01-05 18:37:55 -0700

    Add route to /solve

--- srvr/routes.go
+++ srvr/routes.go
@@ -7,5 +7,6 @@ func (s *Srvr) Routes() {
 	s.Router.HandleFunc("/index.html", s.handleIndex())
 	s.Router.HandleFunc("/form", s.handleForm())
 	s.Router.HandleFunc("/jumble", s.handleJumble())
+	s.Router.HandleFunc("/solve", s.handleSolve())
 	s.Router.HandleFunc("/", s.handleIndex())
 }
